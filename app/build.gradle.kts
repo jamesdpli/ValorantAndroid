2463dc5abc23b04c251c77cd29f2bbb0c722af89
James <96268466+jamesdpli@users.noreply.github.com>
2023-08-31 17:54:56 +0100

    Basic compose navigation template (#6)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,9 @@ android {
 }
 
 dependencies {
+    // Navigation
+    implementation("androidx.navigation:navigation-compose:2.7.1")
+
     // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
