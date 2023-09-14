f6bc7e6d220f3f0adb9972690f4e0d41272be873
James <96268466+jamesdpli@users.noreply.github.com>
2023-09-14 08:19:47 +0100

    Set up nested NavGraph (#13)

--- app/src/main/java/com/example/valorantandroid/ui/core/ValorantApp.kt
+++ app/src/main/java/com/example/valorantandroid/ui/core/ValorantApp.kt
@@ -4,9 +4,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.example.valorantandroid.feature.agent.AgentDetailsScreen
@@ -17,25 +20,40 @@ import com.example.valorantandroid.feature.agent.AgentsViewModel
 @Composable
 fun ValorantApp() {
     val navController = rememberNavController()
+
+    fun NavGraphBuilder.agentsNavGraph(navController: NavController) {
+        navigation(
+            route = "agents",
+            startDestination = "agentsList"
+        ) {
+            composable(
+                route = "agentsList"
+            ) {
+                val viewModel = hiltViewModel<AgentsViewModel>()
+                val agentsUiState by viewModel.agentsScreenUiState.collectAsState()
+                AgentsScreen(
+                    agentsUiState = agentsUiState,
+                    onAgentClicked = { navController.navigate("agent/$it") }
+                )
+            }
+            composable(
+                route = "agent/{agentUuid}",
+                arguments = listOf(navArgument("agentUuid") { type = NavType.StringType })
+            ) {
+                val viewModel = hiltViewModel<AgentDetailsViewModel>()
+                val agentDetailsUiState by viewModel.agentDetailsUiState.collectAsState()
+
+                AgentDetailsScreen(
+                    agentDetailsUiState = agentDetailsUiState
+                )
+            }
+        }
+    }
+
     NavHost(
         navController = navController,
         startDestination = "agents"
     ) {
-        composable(
-            route = "agents"
-        ) {
-            val viewModel = hiltViewModel<AgentsViewModel>()
-            val agentsUiState by viewModel.agentsScreenUiState.collectAsState()
-            AgentsScreen(agentsUiState, {navController.navigate("agent/$it")})
-        }
-        composable(
-            route = "agent/{agentUuid}",
-            arguments = listOf(navArgument("agentUuid") { type = NavType.StringType })
-        ) {
-            val viewModel = hiltViewModel<AgentDetailsViewModel>()
-            val agentDetailsUiState by viewModel.agentDetailsUiState.collectAsState()
-
-            AgentDetailsScreen(agentDetailsUiState = agentDetailsUiState)
-        }
+        agentsNavGraph(navController = navController)
     }
 }
