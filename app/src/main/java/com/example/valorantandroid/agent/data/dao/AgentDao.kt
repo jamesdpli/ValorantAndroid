addd3010813f17fb34f1c2f34e1110481d25ecca
James <jamesdudhiprice@gmail.com>
2024-01-04 16:46:37 +0000

    Add persistent favourite agents part one

--- app/src/main/java/com/example/valorantandroid/agent/data/dao/AgentDao.kt
+++ app/src/main/java/com/example/valorantandroid/agent/data/dao/AgentDao.kt
@@ -1,6 +1,7 @@
 package com.example.valorantandroid.agent.data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
@@ -8,9 +9,12 @@ import com.example.valorantandroid.agent.data.model.local.AgentEntity
 
 @Dao
 interface AgentDao {
-    @Query("SELECT * FROM agents")
+    @Query("SELECT * FROM favourite_agents")
     fun getAllAgents(): List<AgentEntity>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAgent(agentEntity: AgentEntity)
+
+    @Delete
+    fun deleteAgent(agentEntity: AgentEntity)
 }
--- app/src/main/java/com/example/valorantandroid/agent/domain/mapper/AgentNetworkMapper.kt
+++ app/src/main/java/com/example/valorantandroid/agent/domain/mapper/AgentNetworkMapper.kt
@@ -1,5 +1,6 @@
 package com.example.valorantandroid.agent.domain.mapper
 
+import com.example.valorantandroid.agent.data.model.local.AgentEntity
 import com.example.valorantandroid.agent.data.model.network.AgentDetailsNetworkModel
 import com.example.valorantandroid.agent.data.model.network.AgentsNetworkModel
 import com.example.valorantandroid.agent.domain.model.AgentDomainModel
@@ -15,6 +16,21 @@ fun AgentsNetworkModel.Agent.toDomainModel() : AgentDomainModel = AgentDomainMod
 fun AgentDetailsNetworkModel.Agent.toDomainModel(): AgentDomainModel = AgentDomainModel(
     uuid = this.uuid,
     name = this.displayName,
+    description = this.description,
+    displayIcon = this.displayIcon,
+    fullPortrait = this.fullPortrait
+)
+
+fun AgentEntity.toDomainModel(): AgentDomainModel = AgentDomainModel(
+    uuid = this.uuid,
+    name = this.name,
+    description = this.description,
+    displayIcon = this.displayIcon,
+    fullPortrait = this.fullPortrait
+)
+fun AgentDomainModel.toEntity(): AgentEntity = AgentEntity(
+    uuid = this.uuid,
+    name = this.name,
     description = this.description,
     displayIcon = this.displayIcon,
     fullPortrait = this.fullPortrait
