eeab81cb70a76deef538f0ce582d85ee1cadda76
James <jamesdudhiprice@gmail.com>
2024-01-11 12:09:32 +0000

    Make use of function reference

--- app/src/main/java/com/example/valorantandroid/agent/data/repository/AgentsRepository.kt
+++ app/src/main/java/com/example/valorantandroid/agent/data/repository/AgentsRepository.kt
@@ -2,6 +2,7 @@ package com.example.valorantandroid.agent.data.repository
 
 import com.example.valorantandroid.agent.data.dao.AgentDao
 import com.example.valorantandroid.agent.data.model.local.AgentEntity
+import com.example.valorantandroid.agent.data.model.network.AgentsNetworkModel
 import com.example.valorantandroid.agent.data.service.ValorantAgentService
 import com.example.valorantandroid.agent.domain.mapper.toDomainModel
 import com.example.valorantandroid.agent.domain.mapper.toEntity
@@ -15,7 +16,7 @@ class AgentsRepository @Inject constructor(
     suspend fun getAgentsFromNetwork(): List<AgentDomainModel> = valorantService
         .getAgents()
         .agents
-        .map { it.toDomainModel() }
+        .map(AgentsNetworkModel.Agent::toDomainModel)
 
     suspend fun getAgentByUuidFromNetwork(uuid: String): AgentDomainModel = valorantService
         .getAgentByUuid(uuid)
