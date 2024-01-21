d8292a967623a34d899275789c7153c317aef966
James <jamesdudhiprice@gmail.com>
2024-01-21 16:59:16 +0000

    Add comment

--- app/src/test/java/com/example/valorantandroid/utils/fake/FakeAgentsService.kt
+++ app/src/test/java/com/example/valorantandroid/utils/fake/FakeAgentsService.kt
@@ -5,6 +5,10 @@ import com.example.valorantandroid.agent.data.model.network.AgentsNetworkModel
 import com.example.valorantandroid.agent.data.service.ValorantAgentService
 import com.example.valorantandroid.utils.TestUtils
 
+/**
+ * FakeAgentsService acting as a test double (a fake) for AgentsRepositoryImpl.
+ * This repository takes no arguments in its constructor, this is recommended by google.
+ */
 class FakeAgentsService : ValorantAgentService {
 
     override suspend fun getAgents(): AgentsNetworkModel = TestUtils.networkAgents
